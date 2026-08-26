626d88a1ecc4a4d922c484945e81b207b1d4e3ac
Ahmad-Saeed310 <f23bdocs1e02215@iub.edu.pk>
2026-08-26 21:00:31 +0500

    learning objects on 26August

---
+++ Javascript/objects4.js
@@ -0,0 +1,23 @@
+// // making an object 
+// const object={
+//   Student :"Rashid"   // property
+// }
+
+const classof10 ={
+    student1 :{
+    name:"Shahid",
+    id:4,
+    classes:5
+    },
+studies(){
+    return   `Hello this is my name ${this.student1.name}`
+}
+}
+
+
+console.log(classof10.studies())
+// console.log(classof10.student1) 
+
+
+
+
